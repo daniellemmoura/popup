11ff8e3772979903b4a81601d5e4aea9979b1efc
daniellemmoura <daniellemdemoura@gmail.com>
2021-10-19 19:52:42 -0300

    Adiciona ajustes

--- popup.js
+++ popup.js
@@ -1,3 +1,5 @@
+const fields = document.querySelectorAll("[required]")
+
 function openPopup(){
     let popUp = document.querySelector(".popup");
     
@@ -11,22 +13,94 @@ function init() {
 
 function closePopup() {
     let popUp = document.querySelector(".popup");
-
+    
     popUp.classList.remove('show');
 }
 
-function validationPopUp() {
-    let nome = document.querySelector('.form__text__input');
-    const pError = 'O nome precisa ter mais de dois caracteres';
-    if (nome.lenght == 0){
+function ValidateField(field) {
+    // logica para verificar se existem erros
+    function verifyErrors() {
+        let foundError = false;
+
+        for(let error in field.validity) {
+            // se não for customError
+            // então verifica se tem erro
+            if (field.validity[error] && !field.validity.valid ) {
+                foundError = error
+            }
+        }
+        return foundError;
+    }
+
+    function customMessage(typeError) {
+        const messages = {
+            text: {
+                valueMissing: "Por favor, preencha este campo"
+            },
+            email: {
+                valueMissing: "Email é obrigatório",
+                typeMismatch: "Por favor, preencha um email válido"
+            }
+        }
+
+        return messages[field.type][typeError]
+    }
+
+    function setCustomMessage(message) {
+        const spanError = field.querySelector("span.error")
         
+        if (message) {
+            spanError.classList.add("active")
+            spanError.innerHTML = message
+        } else {
+            spanError.classList.remove("active")
+            spanError.innerHTML = ""
+        }
+    }
+
+    return function() {
+
+        const error = verifyErrors()
+
+        if(error) {
+            const message = customMessage(error)
+
+            field.style.borderColor = "red"
+            setCustomMessage(message)
+        } else {
+            field.style.borderColor = "green"
+            setCustomMessage()
+        }
     }
 }
+
+
+function customValidation(event) {
+
+    const field = event.target
+    const validation = ValidateField(field)
+
+    validation()
+
+}
+
+for( field of fields ){
+    field.addEventListener("invalid", event => { 
+        // eliminar o bubble
+        event.preventDefault()
+
+        customValidation(event)
+    })
+    field.addEventListener("blur", customValidation)
+}
+
 
 document.addEventListener('DOMContentLoaded', function () {
     init();
 });
 
 document.addEventListener("", function () {
-    validationPopUp();
+    ValidateField();
 });
+
+
